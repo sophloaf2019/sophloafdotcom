b9ecf83d2e0d4836218218fcd5d3eebaf796bd1e
sophie <sophloaf2019@proton.me>
2026-01-01 20:36:49 -0800

    Style redesign :)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,36 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
+import { Blog } from './blog/blog';
+import { Music } from './music/music';
+import { Code } from './code/code';
+import { About } from './about/about';
+import { Writing } from './writing/writing';
 
 export const routes: Routes = [
 	{
 		path: '',
 		component: Homepage,
+		children: [
+			{
+				path: '',
+				component: About,
+			},
+			{
+				path: 'blog',
+				component: Blog,
+			},
+			{
+				path: 'writing',
+				component: Writing,
+			},
+			{
+				path: 'music',
+				component: Music,
+			},
+			{
+				path: 'code',
+				component: Code,
+			},
+		],
 	},
 ];
